3023fc6be6c2dbc5e3ec084eda0345421f943a53
thunder04 <70254851+thunder04@users.noreply.github.com>
2025-03-16 06:00:52 +0200

    Initial commit

---
+++ game_of_life-web-native-lib/src/lib.rs
@@ -0,0 +1,19 @@
+#[macro_use]
+extern crate tracing;
+
+mod error;
+mod utils;
+
+pub use error::{Error, Result};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    debug!("Called greet function");
+    alert("Hello, bbb!");
+}
